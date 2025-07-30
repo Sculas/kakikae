19c7748d989ea002a7d7aa1009cad4f81971401b
Sculas <contact@sculas.xyz>
2025-07-30 19:37:40 +0200

    refactor: add extra print

--- crates/stage1/src/lk/boot_menu.rs
+++ crates/stage1/src/lk/boot_menu.rs
@@ -24,11 +24,12 @@ install_hooks!(install_hooks; (LK_BASE, LK_SIZE) {
     boot_mode_select_hook(orig: _),
 });
 
-const MT65XX_BOOT_MENU_KEY: c_ushort = 17;
-const MT65XX_MENU_OK_KEY: c_ushort = 0;
+const MT65XX_BOOT_MENU_KEY: c_ushort = 17; // VOL_UP key
+const MT65XX_MENU_OK_KEY: c_ushort = 0; // VOL_DOWN key
 
 unsafe fn boot_mode_select_hook(orig: orig_boot_mode_select_hook) {
     eprintln!("LK: boot_mode_select_hook");
+    video_println!("hello from LK boot menu hook!");
 
     let begin = get_timer(0);
     while get_timer(begin) < 50 {
